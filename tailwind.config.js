6b4c8ba485284e13b6b6d2c82eef5ee9f9cd14a1
vincecommerce <vincenzow0rk@outlook.com>
2022-09-29 12:30:57 +0200

    html code complete

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+  content: ['./index.html', './js/script.js'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+}
